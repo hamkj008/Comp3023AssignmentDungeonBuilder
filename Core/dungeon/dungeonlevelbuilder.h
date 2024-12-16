3ecf2fbaf4f98e93510a91670ecb71d90f014b4f
hamkj008 <kieran.h01@protonmail.com>
2024-12-17 02:24:09 +1030

    commit

--- Core/dungeon/dungeonlevelbuilder.h
+++ Core/dungeon/dungeonlevelbuilder.h
@@ -20,11 +20,11 @@ public:
 
     enum class MoveConstraints : unsigned
     {
-        None = 0,
-        OriginImpassable = 1,
-        DestinationImpassable = 2,
-        OriginLocked = 4,
-        DestinationLocked = 8
+        None                    = 0,
+        OriginImpassable        = 1,
+        DestinationImpassable   = 2,
+        OriginLocked            = 4,
+        DestinationLocked       = 8
     };
 
     virtual void buildDungeonLevel(std::string name, int width, int height);
