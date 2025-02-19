66be33cc5a2f6f8d5282e2ab54ab3cf2c9d08173
hamkj008 <kieran.h01@protonmail.com>
2025-02-20 02:05:56 +1030

    commit

--- Core/dungeon/common/blockeddoorway.cpp
+++ Core/dungeon/common/blockeddoorway.cpp
@@ -4,32 +4,32 @@
 namespace core::dungeon::common {
 
 
-BlockedDoorway::BlockedDoorway()
-    : _description{"Blocked Doorway"},
-      _character{'X'},
-      _passage{false}
-{
+    BlockedDoorway::BlockedDoorway()
+        : _description{"Blocked Doorway"},
+          _character{'X'},
+          _passage{false}
+    {
 
-}
+    }
 
-std::string BlockedDoorway::description() const {
-    return _description;
-}
+    std::string BlockedDoorway::description() const {
+        return _description;
+    }
 
-char BlockedDoorway::displayCharacter() {
-    return _character;
-}
+    char BlockedDoorway::displayCharacter() {
+        return _character;
+    }
 
-void BlockedDoorway::setPassage(bool passage) {
-    _passage = passage;
-}
+    void BlockedDoorway::setPassage(bool passage) {
+        _passage = passage;
+    }
 
-bool BlockedDoorway::isPassage() const {
-    return _passage;
-}
+    bool BlockedDoorway::isPassage() const {
+        return _passage;
+    }
 
 
-std::ostream& BlockedDoorway::operator <<(std::ostream &display) {
-    return display << "a " << description() << " to another chamber";
-}
+    std::ostream& BlockedDoorway::operator <<(std::ostream &display) {
+        return display << "a " << description() << " to another chamber";
+    }
 }
--- Core/dungeon/common/blockeddoorway.h
+++ Core/dungeon/common/blockeddoorway.h
@@ -5,39 +5,39 @@
 
 namespace core::dungeon::common {
 
-/**
- * @brief The BlockedDoorway class
- * Concrete Door type class
- */
-class BlockedDoorway final : public Doorway
-{
-public:
-    BlockedDoorway();
-
-
-    virtual std::string description() const override;
-    virtual char displayCharacter() override;
-
     /**
-     * @brief isPassage
-     * @return true if connected as a Passage.
-     * If connected to another doorway type, the connection becomes a Passage.
+     * @brief The BlockedDoorway class
+     * Concrete Door type class
      */
-    virtual bool isPassage() const override;
-    virtual void setPassage(bool passage) override;
+    class BlockedDoorway final : public Doorway
+    {
+        public:
+            BlockedDoorway();
 
-    /**
-     * @brief operator <<
-     * @param display
-     * @return
-     * Formats the object to be described in the describe menu.
-     */
-    virtual std::ostream& operator <<(std::ostream &display) override;
 
-private:
-    std::string _description;
-    char _character;
-    bool _passage;
-};
+            virtual std::string description() const override;
+            virtual char displayCharacter() override;
+
+            /**
+             * @brief isPassage
+             * @return true if connected as a Passage.
+             * If connected to another doorway type, the connection becomes a Passage.
+             */
+            virtual bool isPassage() const override;
+            virtual void setPassage(bool passage) override;
+
+            /**
+             * @brief operator <<
+             * @param display
+             * @return
+             * Formats the object to be described in the describe menu.
+             */
+            virtual std::ostream& operator <<(std::ostream &display) override;
+
+        private:
+            std::string _description;
+            char _character;
+            bool _passage;
+    };
 }
 #endif // BLOCKEDDOORWAY_H
